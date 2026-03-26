074ec2f3c935af1050f0fb4ceb9e6cd9acb50475
Rahman <136003890+lhlRahman@users.noreply.github.com>
2026-03-26 19:21:36 -0400

    Fix fahrenheit conversion without changing type

--- challenges/easy/lambda_function1.py
+++ challenges/easy/lambda_function1.py
@@ -17,7 +17,7 @@ def lambda_handler(event, context=None):
         }
     
     fahrenheit = (celsius * 9/5) + 32
-    fahrenheit = int(fahrenheit)
+    fahrenheit = fahrenheit
     
     return {
         'statusCode': 200,
