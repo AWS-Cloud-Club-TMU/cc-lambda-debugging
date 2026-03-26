4aafe32294833fb79703c088b605f4c1aa9f3ef5
Srushti <patelsrushtij@gmail.com>
2026-03-26 19:31:35 -0400

    Update lambda_function1.py

--- challenges/easy/lambda_function1.py
+++ challenges/easy/lambda_function1.py
@@ -8,11 +8,11 @@ Expected output: {"statusCode": 200, "body": 77}
 import json
 
 def lambda_handler(event, context=None):
-    celsius = event['temp']
+    celsius = event['temperature']
     
     if celsius == None:
         return {
-            'statusCode': 200,
+            'statusCode': 400,
             'body': json.dumps('Error: temperature field is required')
         }
     
